7cddc40c2e3e84572a74396e35158da9bf78e08d
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-31 21:13:26 +0000

    Added info for sample-test.js

--- contracts/ModifyVariable.sol
+++ contracts/ModifyVariable.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.4;
 
 contract ModifyVariable {
-  uint public x;
+  uint public x; // declare x as a public variable of type uint
 
   constructor(uint _x) {
-    x = _x;
+    x = _x; // set x to _x
   }
 
   function modifyToLeet() public {
-    x = 1337;
+    x = 1337;// modify x to 1337
   }
 
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -6,18 +6,18 @@ const { expect, assert } = require("chai");
 describe("TestModifyVariable", function () {
 	it("should change x to 1337", async function () {
 		// this line creates an ethers ContractFactory abstraction: https://docs.ethers.org/v5/api/contract/contract-factory/
-		const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
+		const ModifyVariable = await ethers.getContractFactory("ModifyVariable"); // this is the name of the contract in the .sol file
 
 		// we then use the ContractFactory object to deploy an instance of the contract
-		const contract = await ModifyVariable.deploy(10);
+		const contract = await ModifyVariable.deploy(10); // 10 is the initial value of x
 
 		// wait for contract to be deployed and validated!
-		await contract.deployed();
+		await contract.deployed(); // this is a promise
 
 		// modify x from 10 to 1337 via this function!
 		await contract.modifyToLeet();
 		// getter for state variable x
-		const newX = await contract.x();
-		assert.equal(newX.toNumber(), 1337);
+		const newX = await contract.x(); // this is a promise
+		assert.equal(newX.toNumber(), 1337); // this is a promise
 	});
 });
